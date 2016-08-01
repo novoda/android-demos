39e37bc7ab68b7bea69a612cbf0eadc793cf69c4
Xavi Rigau <xavi@novoda.com>
2016-08-01 12:25:26 +0200

    Add basic skeletton for the chips view demo

---
+++ EditTextChips/README.md
@@ -0,0 +1,5 @@
+#Demo EditTextChips
+
+EditTextChips shows different libraries to display 'chips' in an EditText (usually used to show tags, email addresses, etc.).
+
+// TODO explain what libraries we use
---
+++ EditTextChips/src/main/java/com/novoda/demo/edittextchips/MainActivity.java
@@ -0,0 +1,49 @@
+package com.novoda.demo.edittextchips;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.eyeem).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+        findViewById(R.id.klinker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+        findViewById(R.id.doodle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+        findViewById(R.id.plumillon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+        findViewById(R.id.splitwise).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+    }
+
+}
