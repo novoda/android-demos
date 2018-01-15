f0c8d7cb64a2e37886f53269bacfc48efbc1acba
Luis G. Valle <luisgvalle@gmail.com>
2018-01-15 15:10:32 +0000

    Log received message

--- FirebaseNotifications/app/src/main/java/com/novoda/demo/firebasenotifications/messaging/MessagingService.kt
+++ FirebaseNotifications/app/src/main/java/com/novoda/demo/firebasenotifications/messaging/MessagingService.kt
@@ -1,11 +1,13 @@
 package com.novoda.demo.firebasenotifications.messaging
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        Log.d("MessagingService", "Message received: "+remoteMessage.toString())
         super.onMessageReceived(remoteMessage)
     }
 }
