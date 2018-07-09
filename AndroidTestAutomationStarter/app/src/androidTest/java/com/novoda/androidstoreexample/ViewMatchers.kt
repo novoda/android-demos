a6abf1ee48e824facb8707fb13b97c9459a01a35
Dimitris Karavias <karavias.d@gmail.com>
2018-07-09 18:28:00 +0300

    ViewMatchers  cleaning up

--- AndroidTestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/ViewMatchers.kt
+++ AndroidTestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/ViewMatchers.kt
@@ -13,14 +13,11 @@ object ViewMatchers {
     fun withCategoryTitle(title: String): Matcher<RecyclerView.ViewHolder> {
         return object : BoundedMatcher<RecyclerView.ViewHolder, CategoryAdapter.Holder>(CategoryAdapter.Holder::class.java) {
             override fun matchesSafely(item: CategoryAdapter.Holder): Boolean {
-                if (item.categoryName != null) {
-                    return item.categoryName!!.text.toString().equals(title, true)
-                }
-                return false
+                return item.categoryName?.let { it.text.toString().equals(title, true) } ?: false
             }
 
             override fun describeTo(description: Description) {
-                description.appendText("view holder with title: " + title)
+                description.appendText("view holder with title: $title")
             }
         }
     }
@@ -29,14 +26,11 @@ object ViewMatchers {
     fun withProductTitle(title: String): Matcher<RecyclerView.ViewHolder> {
         return object : BoundedMatcher<RecyclerView.ViewHolder, ProductListAdapter.Holder>(ProductListAdapter.Holder::class.java) {
             override fun describeTo(description: Description) {
-                description.appendText("view holder with title: " + title)
+                description.appendText("view holder with title: $title")
             }
 
             override fun matchesSafely(item: ProductListAdapter.Holder?): Boolean {
-                if (item?.productName != null) {
-                    return item.productName!!.text.toString().equals(title, true)
-                }
-                return false
+                return item?.productName?.let { it.text.toString().equals(title, true) } ?: false
             }
         }
     }
