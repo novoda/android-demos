7bd478090bf922df85f8c302ccfb026441c6c913
Dimitris Karavias <karavias.d@gmail.com>
2018-07-10 14:32:58 +0300

    Refactored ProductListInteractorImpl to better follow Kotlin syntax

--- AndroidTestAutomationStarter/app/src/main/java/com/novoda/androidstoreexample/mvp/interactor/impl/ProductListInteractorImpl.kt
+++ AndroidTestAutomationStarter/app/src/main/java/com/novoda/androidstoreexample/mvp/interactor/impl/ProductListInteractorImpl.kt
@@ -15,20 +15,21 @@ class ProductListInteractorImpl @Inject constructor(val retrofit: Retrofit, priv
     private var call: Call<ProductResponse>? = null
 
     override fun loadProductList(produListListener: ProductListListener, category: Int) {
-        call = apiService.getProductsFromCategory(category)
-        call?.enqueue(object : Callback<ProductResponse> {
-            override fun onFailure(call: Call<ProductResponse>?, t: Throwable?) {
-                produListListener.onFailure("Error while fetching")
-            }
-
-            override fun onResponse(call: Call<ProductResponse>?, response: Response<ProductResponse>?) {
-                if (response?.isSuccessful!!) {
-                    produListListener.onSuccess(response.body()!!)
-                } else {
-                    produListListener.onFailure("Error while fetching data")
+        call = apiService.getProductsFromCategory(category).apply {
+            enqueue(object : Callback<ProductResponse> {
+                override fun onFailure(call: Call<ProductResponse>, t: Throwable) {
+                    produListListener.onFailure("Error while fetching")
                 }
-            }
-        })
+
+                override fun onResponse(call: Call<ProductResponse>, response: Response<ProductResponse>) {
+                    if (response.isSuccessful) {
+                        produListListener.onSuccess(response.body()!!)
+                    } else {
+                        produListListener.onFailure("Error while fetching data")
+                    }
+                }
+            })
+        }
     }
 
     override fun cancel() {
