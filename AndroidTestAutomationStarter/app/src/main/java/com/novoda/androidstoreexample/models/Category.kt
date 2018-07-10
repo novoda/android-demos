5d603ce260b60eb811f2377d972a194e5a74784c
Dimitris Karavias <karavias.d@gmail.com>
2018-07-10 14:22:01 +0300

    Remove unnecessary toString() implementation

--- AndroidTestAutomationStarter/app/src/main/java/com/novoda/androidstoreexample/models/Category.kt
+++ AndroidTestAutomationStarter/app/src/main/java/com/novoda/androidstoreexample/models/Category.kt
@@ -1,7 +1,3 @@
 package com.novoda.androidstoreexample.models
 
-data class Category(val id: Int, val title: String, val image: String) {
-    override fun toString(): String {
-        return title
-    }
-}
+data class Category(val id: Int, val title: String, val image: String)
