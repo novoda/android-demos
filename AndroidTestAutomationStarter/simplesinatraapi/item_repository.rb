b226bb60cd06397dd50333deadf41d2eae10eba5
Dimitris Karavias <karavias.d@gmail.com>
2018-07-10 14:36:50 +0300

    Added missing ")"

--- AndroidTestAutomationStarter/simplesinatraapi/item_repository.rb
+++ AndroidTestAutomationStarter/simplesinatraapi/item_repository.rb
@@ -6,7 +6,7 @@ class ItemRepository
   def initialize
     @items = { '3': [
         Item.new(1, 'Hat Green', '$18', 'hat1', get_random_description(4)),
-        Item.new(2, 'Hat Black', '$12', 'hat2', get_random_description(2),
+        Item.new(2, 'Hat Black', '$12', 'hat2', get_random_description(2)),
         Item.new(3, 'Hat White', '$11', 'hat3', get_random_description(3)),
         Item.new(4, 'Hat Blue', '$29', 'hat4', get_random_description(1)),
     ],
