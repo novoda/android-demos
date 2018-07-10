1d14e369a8bdc3d39fcb6c9a196ceb40ad201e1b
Dimitris Karavias <karavias.d@gmail.com>
2018-07-10 14:26:31 +0300

    Use primary constructor

--- AndroidTestAutomationStarter/app/src/main/java/com/novoda/androidstoreexample/dagger/categoryList/CategoryListModule.kt
+++ AndroidTestAutomationStarter/app/src/main/java/com/novoda/androidstoreexample/dagger/categoryList/CategoryListModule.kt
@@ -1,20 +1,15 @@
 package com.novoda.androidstoreexample.dagger.categoryList
 
-import dagger.Module
-import dagger.Provides
 import com.novoda.androidstoreexample.mvp.interactor.CategoryListInteractor
 import com.novoda.androidstoreexample.mvp.interactor.impl.CategoryListInteractorImpl
 import com.novoda.androidstoreexample.mvp.presenter.CategoryListPresenter
 import com.novoda.androidstoreexample.mvp.presenter.impl.CategoryListPresenterImpl
 import com.novoda.androidstoreexample.mvp.view.CategoryListView
+import dagger.Module
+import dagger.Provides
 
 @Module
-class CategoryListModule {
-    val categoryListView: CategoryListView
-
-    constructor(categoryListView: CategoryListView) {
-        this.categoryListView = categoryListView
-    }
+class CategoryListModule(val categoryListView: CategoryListView) {
 
     @Provides
     fun providesView(): CategoryListView = categoryListView
