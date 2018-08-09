7dca1d8be154de6954f04b9b4de1d73a73fa0286
lgvalle <luis.gvalle@gmail.com>
2018-08-09 17:10:07 +0100

    Add junit test support

---
+++ MoviesExample/core/src/test/java/com/novoda/demo/movies/MovieServiceTest.java
@@ -0,0 +1,14 @@
+package com.novoda.demo.movies;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class MovieServiceTest {
+
+    @Test
+    public void testsCanRunAndFail() {
+        assertThat(true).isFalse();
+    }
+
+}
