5521fd300d1e2a261fc743de9a56cd0e883cdd83
Sven Kroell <sven@novoda.com>
2018-10-02 13:27:38 +0200

    add model and simple repository

---
+++ AndroidTestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/data/repositories/ArticleRepository.kt
@@ -0,0 +1,9 @@
+package com.novoda.androidstoreexample.data.repositories
+
+import com.novoda.androidstoreexample.data.models.Article
+
+class ArticleRepository {
+    val standardArticle = Article(title = "Hat white", category = "HATS")
+
+
+}
