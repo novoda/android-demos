bc9b5264405b6b5bd0016d7cd0a5a2681d314a1e
Sven Kroell <sven@novoda.com>
2018-10-02 12:55:19 +0200

    add methods for json response

--- AndroidTestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/rules/MockServerTestRule.kt
+++ AndroidTestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/rules/MockServerTestRule.kt
@@ -1,16 +1,19 @@
 package com.novoda.androidstoreexample.rules
 
 import android.app.Activity
+import android.support.test.InstrumentationRegistry
 import android.support.test.rule.ActivityTestRule
 import com.novoda.androidstoreexample.EspressoHostModule
 import com.novoda.androidstoreexample.dagger.App
 import com.novoda.androidstoreexample.dagger.component.DaggerAppComponent
+import com.squareup.okhttp.mockwebserver.MockResponse
 import com.squareup.okhttp.mockwebserver.MockWebServer
+import java.io.InputStream
 
-class MockServerTestRule<T: Activity>(
+class MockServerTestRule<T : Activity>(
         activityClass: Class<T>?,
         initialTouchMode: Boolean,
-        launchActivity: Boolean): ActivityTestRule<T>(activityClass, initialTouchMode, launchActivity) {
+        launchActivity: Boolean) : ActivityTestRule<T>(activityClass, initialTouchMode, launchActivity) {
 
     internal val mockWebServer = MockWebServer()
     private val port = 9091
@@ -26,4 +29,18 @@ class MockServerTestRule<T: Activity>(
         mockWebServer.shutdown()
         super.afterActivityFinished()
     }
+
+    internal fun encueJson(resourceName: String) {
+        mockWebServer.enqueue(createResponseFor(resourceName))
+    }
+
+    private fun convertStreamToString(input: InputStream): String {
+        return input.bufferedReader().use { it.readText() }
+    }
+
+    private fun createResponseFor(resourceName: String): MockResponse {
+        val resources = InstrumentationRegistry.getContext().resources.assets
+        return MockResponse().setBody(convertStreamToString(resources.open(resourceName)))
+    }
+
 }
