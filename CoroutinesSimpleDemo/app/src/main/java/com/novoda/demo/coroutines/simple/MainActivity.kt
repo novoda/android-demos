d4758542af4f18e0f69a3a3a5af3217e8561538d
Juanky Soriano <juanky@novoda.com>
2019-08-21 18:17:56 +0200

    Achieve session 2
    // 1. On activity start we see a loading on main screen
    // 2. Loading while a 2 seconds background operation completes
    // 3. After completing background operation print "DONE" on screen
    // 4. Move loading / background job to separate components

--- CoroutinesSimpleDemo/app/src/main/java/com/novoda/demo/coroutines/simple/MainActivity.kt
+++ CoroutinesSimpleDemo/app/src/main/java/com/novoda/demo/coroutines/simple/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.novoda.demo.coroutines.simple
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
@@ -18,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        check_box.setOnClickListener {
+            Log.e("COROUTINES", "CANCELLING")
+            viewModel.cancel()
+        }
+
     }
 
     override fun onStart() {
@@ -28,16 +34,19 @@ class MainActivity : AppCompatActivity() {
 }
 
 private class ViewModel(private val callback: (String) -> Unit) {
+    private var job: Job? = null
     private val uiScope = CoroutineScope(Dispatchers.Main)
     private val service = Service()
 
     fun load() {
-        uiScope.launch {
+        job = uiScope.launch {
             callback("LOADING")
             val job = service.backgroundJobAsync()
             callback(job.await())
         }
     }
+
+    fun cancel() = job?.cancel()
 
 }
 
@@ -45,7 +54,12 @@ private class Service {
     suspend fun backgroundJobAsync() = withContext(Dispatchers.IO) {
         async {
             Thread.sleep(5000)
-            "DONE"
+            if (this.isActive) {
+                Log.e("COROUTINES", "JOB")
+                "DONE"
+            } else {
+                ""
+            }
         }
     }
 }
