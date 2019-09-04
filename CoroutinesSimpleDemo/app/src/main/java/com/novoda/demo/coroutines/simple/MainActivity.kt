2deef0188a3d4658418571b9afba00188778e41d
pablisco <pabs87@gmail.com>
2019-09-04 17:24:27 +0100

    introducing lifecycle

--- CoroutinesSimpleDemo/app/src/main/java/com/novoda/demo/coroutines/simple/MainActivity.kt
+++ CoroutinesSimpleDemo/app/src/main/java/com/novoda/demo/coroutines/simple/MainActivity.kt
@@ -3,6 +3,7 @@ package com.novoda.demo.coroutines.simple
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 
@@ -12,20 +13,28 @@ import kotlinx.coroutines.*
 // 3. After completing background operation print "DONE" on screen
 // 4. Move loading / background job to separate components
 // 5. Create a second service which will run in parallel and print "DONE IN PARALLEL" on screen
+// 6. Cancelling scope
 
 class MainActivity : AppCompatActivity() {
 
-    private var viewModel: ViewModel = ViewModel(
-        main_text_view.text = it,
-        second_text_view.text = it
-    )
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.firstResult.observe(this, Observer {
+            main_text_view.text = it
+        })
+        viewModel.secondResult.observe(this, Observer {
+            second_text_view.text = it
+        })
+
         check_box.setOnClickListener {
             Log.e("COROUTINES", "CANCELLING")
-            viewModel.cancel()
+            viewModel.viewModelScope.cancel()
         }
 
     }
@@ -37,21 +46,30 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-private class ViewModel(private val callback: (String) -> Unit,
-                        private val secondCallback: (String) -> Unit) {
-    private var job: Job? = null
-    private val uiScope = CoroutineScope(Dispatchers.Main)
+class MainViewModel : ViewModel() {
+
+    private val _firstResult = MutableLiveData<String>()
+    val firstResult: LiveData<String> = _firstResult
+    private val _secondResult = MutableLiveData<String>()
+    val secondResult: LiveData<String> = _secondResult
+
     private val service = Service()
+    private val secondService = SecondService()
 
     fun load() {
-        job = uiScope.launch {
-            callback("LOADING")
+        viewModelScope.launch {
+            _firstResult.value = "LOADING"
             val job = service.backgroundJobAsync()
-            callback(job.await())
+            _firstResult.value = job.await()
+        }
+        viewModelScope.launch {
+            _secondResult.value = "ALSO LOADING"
+            val job = secondService.backgroundJobAsync()
+            _secondResult.value = job.await()
         }
     }
 
-    fun cancel() = job?.cancel()
+//    fun cancel() = uiScope?.cancel()
 
 }
 
@@ -70,7 +88,7 @@ private class Service {
 }
 
 private class SecondService {
-    suspend fun backgroundJobAsync() = withContext(Dispathers.IO) {
+    suspend fun backgroundJobAsync() = withContext(Dispatchers.IO) {
         async {
             Thread.sleep(10000)
             if (this.isActive) {
