1b7b093328e7c90b857fed5caad22708c895ac1a
Tobias Heine <tobias@novoda.com>
2019-09-12 10:20:48 +0200

    Merge view model scoped disposable into a CompositDisposable

--- ModelViewIntentSample/search/src/main/java/com/novoda/movies/mvi/search/presentation/SearchViewModel.kt
+++ ModelViewIntentSample/search/src/main/java/com/novoda/movies/mvi/search/presentation/SearchViewModel.kt
@@ -6,42 +6,34 @@ import androidx.lifecycle.ViewModel
 import com.novoda.movies.mvi.search.BaseStore
 import com.novoda.movies.mvi.search.domain.SearchReducer
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 internal typealias SearchStore = BaseStore<SearchViewModel.Action, SearchViewModel.State, SearchReducer.Changes>
 
 internal class SearchViewModel(private val store: SearchStore) : ViewModel() {
-    private val wireDisposable = store.wire()
+    private val viewModelScopeDisposable = CompositeDisposable()
     private var bindDisposable: Disposable? = null
-    private var stateDisposable: Disposable? = null
-
-    init {
-        stateDisposable = store
-            .state
-            .subscribe { state ->
-                _state.postValue(state)
-            }
-    }
-
-
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
-
 
     fun bind(actions: Observable<Action>) {
         bindDisposable = store.bind(actions)
     }
 
-    override fun onCleared() {
-        super.onCleared()
-
-        unbind()
-        wireDisposable.dispose()
-        stateDisposable?.dispose()
+    init {
+        viewModelScopeDisposable.add(store.wire())
+        viewModelScopeDisposable.add(store.state.subscribe { state -> _state.postValue(state) })
     }
 
     fun unbind() = bindDisposable?.dispose()
+
+    override fun onCleared() {
+        super.onCleared()
+
+        viewModelScopeDisposable.clear()
+    }
 
     internal data class State(
         var queryString: String,
